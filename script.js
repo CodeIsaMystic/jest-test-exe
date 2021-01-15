52812dcb6305b71e82f513e073f35e968cc0077e
Julien Orjollet <oj.professionnel@gmail.com>
2021-01-15 09:29:06 +0100

    continuing tests incliuding all tests in describe method

--- script.js
+++ script.js
@@ -12,11 +12,11 @@ const googleSearch = (searchInput, db) => {
   const matches = db.filter(website => {
     return website.includes(searchInput);
   })
-  return matches.length > 3 ? matches.slices(0, 3) : matches;
+  return matches.length > 3 ? matches.slice(0, 3) : matches;
 }
 
-console.log(googleSearch('soup', googleDatabase));
-console.log(googleSearch('cats', googleDatabase));
+//console.log(googleSearch('soup', googleDatabase));
+//console.log(googleSearch('cats', googleDatabase));
 
 
 
--- script.test.js
+++ script.test.js
@@ -7,16 +7,38 @@ const dbMock = [
   'dogpictures.com'
 ];
 
-it('is a silly test', () => {
-  expect('hello').toBe('hello');
-  //googleSearch('test', dbMock);
+
+/*   Entering all tests in describe   */
+describe('googleSearch', () => {
+  /*  Testing simple mode  */
+  it('is a silly test', () => {
+    expect('hello').toBe('hello');
+    //googleSearch('test', dbMock);
+  });
+
+
+  it('is searching google', () => {
+    expect(googleSearch('test thus', dbMock))
+      .toEqual([])
+    expect(googleSearch('dog', dbMock))
+      .toEqual(['dog.com', 'dogpictures.com'])
+  });
+
+
+
+  /*  Testing types with undefined and null  */
+  it('work with undefined and null inputs', () => {
+    expect(googleSearch(undefined, dbMock))
+      .toEqual([])
+    expect(googleSearch(null, dbMock))
+      .toEqual([])
+  });
+
+
+  /*  Testing length of matches  */
+  it('does not return more than 3 matches', () => {
+    expect(googleSearch('.com', dbMock).length).toEqual(3);
+    //expect(googleSearch('.com', dbMock).length).toEqual(4);
+  });
+
 });
-
-
-it('is searching google', () => {
-  expect(googleSearch('test thus', dbMock))
-    .toEqual([])
-  expect(googleSearch('dog', dbMock))
-    .toEqual(['dog.com', 'dogpictures.com'])
-});
-
